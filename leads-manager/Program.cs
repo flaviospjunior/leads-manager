34976fb1722b2f1304b354d8130a9c14e8e4f4e8
flaviospjunior@gmail.com <flaviospjunior@gmail.com>
2024-06-23 02:28:32 -0300

    Habilitando propriedades de anotação do swagger.

--- leads-manager/Program.cs
+++ leads-manager/Program.cs
@@ -3,6 +3,7 @@ using Leads.Data.Configuration;
 using Leads.Data.Contexts;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.EnableAnnotations();
+});
 builder.Services.AddServices();
 
 builder.Services.AddDbContext<LeadsDbContext>(options =>
